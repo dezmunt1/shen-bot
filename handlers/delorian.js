cdbe1f092ddb5f4160ebb9e2c59cad5451c43da8
dezmunt1 <dezmunt1@gmail.com>
2019-04-15 22:20:19 +0300

    fix

--- handlers/delorian.js
+++ handlers/delorian.js
@@ -5,18 +5,8 @@ const Stage = require('telegraf/stage');
 const session = require('telegraf/session');
 const Composer = require('telegraf/composer');
 
-
-/* function sendFuture(ctx) {
-    let msg = ctx.callbackQuery.message;
-    ctx.editMessageText('Залупа коня',  Extra.HTML().markup((m) =>
-    m.inlineKeyboard([
-        m.callbackButton('🚀 Отправить в будущее', 'sendFuture'),
-        m.callbackButton('🔭 Посмотреть в будущее', 'watchFuture'),
-        m.callbackButton('🔙 Вернуть из будущего', 'backFuture')
-        ]))
-    );
-} */
-const mess = {};
+let mess = {};
+let pess = {};
 
 
 module.exports = (ctx, bot) => {
@@ -24,37 +14,64 @@ module.exports = (ctx, bot) => {
         Markup.callbackButton('🚀 Отправить в будущее', 'sendFuture'),
         Markup.callbackButton('🔭 Посмотреть в будущее', 'watchFuture'),
         Markup.callbackButton('🔙 Вернуть из будущего', 'backFuture')
-    ]).extra());
-    console.log(ctx);
+    ]).extra())
+        .then(ctx_then =>{
+            mess['chat_id'] = ctx_then.chat.id;
+            mess['message_id'] = ctx_then.message_id;
+        })
+        .catch(err => console.log(err));
     
 
-    const delorWizard = new WizardScene('delorWizard', (ctx) => {
+    const delorWizard = new WizardScene('delorWizard', (ctx, next) => {
         if (!mess.unperformed) {
-            ctx.editMessageText('Введите отправляемый текст', Markup.inlineKeyboard([
+            ctx.telegram.editMessageText(mess.chat_id, mess.message_id, null, 'Введите отправляемый текст', Markup.inlineKeyboard([
                 Markup.callbackButton('🔙 Назад', 'leaveWizard')]).extra())
-                    .then(ctx =>{
-                        mess['chat_id'] = ctx.chat.id;
-                        mess['message_id'] = ctx.message_id;
+                    .then(ctx_then =>{
+                        mess['chat_id'] = ctx_then.chat.id;
+                        mess['message_id'] = ctx_then.message_id;
                         mess['unperformed'] = 1;
-                    });
+                        console.log(mess);
+                        return ctx.wizard.next();
+                    })
+                    .catch(err => console.log(err));
         } else {
             ctx.telegram.editMessageText(mess.chat_id, mess.message_id, null, 'Должна быть запись в формате ДД.ММ.ГГГГ в ЧЧ.ММ', Markup.inlineKeyboard([
                 Markup.callbackButton('🔙 Назад', 'leaveWizard')]).extra())
-                    .then(ctx =>{
-                        mess['chat_id'] = ctx.chat.id;
-                        mess['message_id'] = ctx.message_id;
+                    .then(ctx_then =>{
+                        mess['chat_id'] = ctx_then.chat.id;
+                        mess['message_id'] = ctx_then.message_id;
                         mess['unperformed'] = 1;
+                        mess['text'] = ctx_then.text;
+                        return ctx.wizard.next();
                     })
-                    .catch(err => console.log(err));
-        }
-        return ctx.wizard.next();
+                    .catch(err => {
+                        if (err.on.payload.text === mess.text) {
+                            return ctx.wizard.next();
+                        }
+                    });
+                    
+        };
+        console.log('Сцена 1', ctx);
+
     },
         (ctx) =>{
+            console.log('Сцена 2');
+            console.log('im here', ctx);
             if(!ctx.message.text.match(/\d{1,2}\.\d{1,2}\.\d{4}\sв\s\d{1,2}\.\d{1,2}/g)){
-                return ctx.wizard.back();
+                pess['chat_id'] = ctx.chat.id;
+                pess['message_id'] = ctx.message.message_id;
+                ctx.deleteMessage(pess.message_id)
+                .catch(err => console.log(err));
+                ctx.wizard.selectStep(1);
+                
+            } else {
+                ctx.reply('Done');
+                mess = {};
+                pess = {};
+                return ctx.scene.leave();
             };
-            ctx.reply('Done');
-            return ctx.scene.leave()
+            console.log(ctx.message.text.match(/\d{1,2}\.\d{1,2}\.\d{4}\sв\s\d{1,2}\.\d{1,2}/g));
+            
         });
     
     const stage = new Stage();
@@ -64,7 +81,8 @@ module.exports = (ctx, bot) => {
 
     bot.action('sendFuture', (ctx) => ctx.scene.enter('delorWizard'));
     bot.action('leaveWizard', (ctx) => {
-        ctx.reply('doneee');
-        return ctx.wizard.back();
+        mess = {};
+        pess = {};
+        return ctx.scene.leave();
     });
 };
